e0f9ab2015cc4658d1bca8007b1e6c7fea14000d
Rafael Cara <cara.rafael@gmail.com>
2025-09-24 13:05:41 -0300

    gera alert com resposta

--- static/js/script.js
+++ static/js/script.js
@@ -2,15 +2,24 @@ const form = document.getElementById("myForm");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const formData = new FormData(form);
 
+  const formData = new FormData(form);
   const protocol = formData.get("protocol");
 
   fetch(`http://127.0.0.1:8000/upload/${protocol}`, {
-    //method: "GET",
     method: "POST",
     body: formData,
   })
-    .then((res) => res.json())
-    .then((res) => console.log(res));
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`Erro: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((data) => {
+      alert(data.message || JSON.stringify(data));
+    })
+    .catch((err) => {
+      alert(`Falha ao enviar: ${err}`);
+    });
 });
